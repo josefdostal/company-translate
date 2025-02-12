6ba893164e9754fc245cfe016ce170d4a0d8bf54
Josef Dostál <josef.dostal@adlerinternational.onmicrosoft.com>
2025-02-12 09:40:52 +0100

    create translation controller

--- api/CompanyTranslate.WebApi/Controllers/Definitions/DefinitionsController.cs
+++ api/CompanyTranslate.WebApi/Controllers/Definitions/DefinitionsController.cs
@@ -6,15 +6,4 @@ namespace CompanyTranslate.WebApi.Controllers.Definitions;
 [Route("api/v1/definitions")]
 public class DefinitionsController : Controller
 {
-	[HttpGet]
-	[Route("{phrase}")]
-	public async Task<IActionResult> GetAsync(string phrase, CancellationToken ct)
-	{
-		if(phrase == "hello")
-			return Json(new
-			          {
-				        Foo = "ahoj"  
-			          });
-		return NotFound();
-	}
 }
---
+++ api/CompanyTranslate.WebApi/Controllers/Translations/TranslationsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyTranslate.WebApi.Controllers.Translations;
+
+
+[Controller]
+[Route("api/v1/translations")]
+public class TranslationsController : ControllerBase
+{
+	[HttpGet]
+	[Route("translations/{phrase}")]
+	public async Task<string> GetTranslationsAsync(string phrase, CancellationToken cancellationToken)
+	{
+		return "";
+	}
+}
