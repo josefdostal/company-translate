928a5735356c0103c9857ea3278c8d7bccefd66f
Josef Dostál <josef.dostal@adlerinternational.onmicrosoft.com>
2025-02-23 03:17:15 +0100

    move the logic to application layer

---
+++ api/CompanyTranslate.Application/Services/Languages/ILanguageService.cs
@@ -0,0 +1,8 @@
+﻿using CompanyTranslate.Application.Dtos.Languages;
+
+namespace CompanyTranslate.Application.Services.Languages;
+
+public interface ILanguageService
+{
+	List<LanguageDto> GetLanguages(bool? isAvailable = null);
+}
--- api/CompanyTranslate.WebApi/Controllers/Langauges/LanguagesController.cs
+++ api/CompanyTranslate.WebApi/Controllers/Langauges/LanguagesController.cs
@@ -1,24 +1,17 @@
-﻿using CompanyTranslate.Application.Configurations;
-using CompanyTranslate.WebApi.Controllers.Langauges.Models;
+﻿using CompanyTranslate.Application.Services.Languages;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 
 namespace CompanyTranslate.WebApi.Controllers.Langauges;
 
 [Controller]
 [Route("/api/v1/languages")]
-public class LanguagesController(IOptions<CompanyTranslateConfiguration> config) : ControllerBase
+public class LanguagesController(ILanguageService service) : ControllerBase
 {
 	[HttpGet]
 	public IActionResult Get([FromQuery]bool? isAvailable)
 	{
-		var result = SupportedLangauge.All
-		             .Select(x => new LanguageResponse(x.Code, x.Name, config.Value.AvailableLanguages.Contains(x.Code)))
-		             .ToList();
+		var result = service.GetLanguages(isAvailable);
 		
-		if(isAvailable != null)
-			result.RemoveAll(x => x.IsAvailable != isAvailable);
-
-		return new OkObjectResult(result);
+		return Ok(result);
 	}
 }
